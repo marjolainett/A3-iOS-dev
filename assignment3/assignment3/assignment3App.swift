80a3c58b0962b833aca58f469c5f5dadaffb16fa
mathildebodet <mathildebodet@orange.fr>
2024-05-03 10:36:01 +1000

    XCode Application

---
+++ assignment3/assignment3/assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment3App.swift
+//  assignment3
+//
+//  Created by Mathilde Bodet on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
