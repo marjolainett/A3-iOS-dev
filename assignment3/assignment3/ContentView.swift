d14d4cfe88a7de5f9b84ef1e80226a0783f26187
mathildebodet <mathildebodet@orange.fr>
2024-05-03 11:24:28 +1000

    add a task

--- assignment3/assignment3/ContentView.swift
+++ assignment3/assignment3/ContentView.swift
@@ -17,12 +17,12 @@ struct ContentView: View {
                     .foregroundColor(.mint)
                     .font(.largeTitle)
                 Spacer()
-//You can click New Game to play the game
+
                 NavigationLink(destination: TodoView(), label: {Text("To-Do List")
                         .font(.title)
                 })
                 .padding(50)
-//You can click High Score to access the 10 best results with the name of the player
+
                 NavigationLink(destination: ReminderView(), label: {Text("Add a reminder")
                         .font(.title)
                 })
